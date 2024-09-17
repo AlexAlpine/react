53f004e8feccd21eed9256bb6fd386bf03318c55
AlexAlpine <alex6122223@gmail.com>
2024-09-17 17:35:40 +0200

    lesson_04

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './lesson_01/Lesson01';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Lesson04 from './lessons/lesson_04/Lesson04';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // мы оборачиваем дочерние теги в родитель по просьбе react
   // если нам не нужен явный контейнер мы можем воспользоваться пустым тегом - react фрагментом
   <>
-    <App />
+    {/* вызов компонента происходит через обращение к нему в теге */}
+    {/* <Lesson01 /> */}
+    {/* <Lesson02 /> */}
+    {/* <Lesson03 /> */}
+    {/* <Homework03 /> */}
+    <Lesson04/>
   </>
 );
